6c421f6e53379d64e0116ce8aa2d04003e61b53c
shuo <1115464444@qq.com>
2020-11-02 19:16:52 +0800

    添加了组件

---
+++ meituan/src/components/DetailTopNav/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>DetailTopNav</div>
+</template>
+
+<script>
+export default {
+  name: 'DetailTopNav',
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ meituan/src/pages/Detail/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="detailContainer">
+    <DetailTopNav></DetailTopNav>
+  </div>
+</template>
+
+<script>
+import DetailTopNav from '@/components/DetailTopNav'
+export default {
+  name: 'Detail',
+  components: {
+    DetailTopNav,
+  },
+}
+</script>
+
+<style lang="less" scoped></style>
