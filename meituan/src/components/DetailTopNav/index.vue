8feee87aa483e17ff87de150b9dc174717f84761
shuo <1115464444@qq.com>
2020-11-02 22:52:54 +0800

    左侧分类筛选右侧猜你喜欢静态完成

--- meituan/src/components/DetailTopNav/index.vue
+++ meituan/src/components/DetailTopNav/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>DetailTopNav</div>
+  <div class="detailTopNavContainer">
+    <div class="breadcrumbs">
+      <span>
+        <a href="javascript:;">北京美团</a>
+      </span>
+      <span>
+        <a href="javascript:;">北京美食</a>
+      </span>
+      <span>
+        <a href="javascript:;">北京美食</a>
+      </span>
+      <span>
+        <a href="javascript:;">北京美食</a>
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +23,26 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+// 面包屑导航
+.detailTopNavContainer {
+  .breadcrumbs {
+    font-size: 12px;
+    color: #222;
+    line-height: 17px;
+    padding: 19px 0;
+    a {
+      text-decoration: none;
+      cursor: pointer;
+      color: #222;
+    }
+    span ::after {
+      content: '>';
+      padding: 0 3px;
+    }
+    span:last-child ::after {
+      content: '';
+    }
+  }
+}
+</style>
--- meituan/src/pages/Detail/index.vue
+++ meituan/src/pages/Detail/index.vue
@@ -1,17 +1,389 @@
 <template>
-  <div class="detailContainer">
-    <DetailTopNav></DetailTopNav>
+  <div>
+    <!-- 使用头部组件 -->
+    <Header></Header>
+    <!-- 商品列表容器 -->
+    <div class="detailContainer">
+      <!-- 面包屑导航 -->
+      <DetailTopNav></DetailTopNav>
+      <!-- 分类区 -->
+      <div class="left">
+        <div class="filter">
+          <div class="condition">
+            <div class="classification">分类</div>
+            <div class="all">
+              <a href="javascript:;" class="active">
+                全部
+              </a>
+            </div>
+            <ul class="con">
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+            </ul>
+          </div>
+          <div class="condition">
+            <div class="classification">分类</div>
+            <div class="all">
+              <a href="javascript:;" class="active">
+                全部
+              </a>
+            </div>
+            <ul class="con">
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+            </ul>
+          </div>
+          <div class="condition">
+            <div class="classification">分类</div>
+            <div class="all">
+              <a href="javascript:;" class="active">
+                全部
+              </a>
+            </div>
+            <ul class="con">
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+              <li>
+                <a href="javascript:;">代金券</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="list">111</div>
+      </div>
+      <!-- 分类区右侧 -->
+      <div class="right">
+        <div class="guess-you-like">
+          <h3>猜你喜欢</h3>
+          <ul>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:;">
+                <div class="pic">
+                  <img
+                    src="https://p1.meituan.net//bbia/f58fafc0be2bc3a69dfcac20260888c2201000.jpg@188w_106h_1e_1c"
+                    alt=""
+                  />
+                </div>
+                <p class="merchant-name">
+                  晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）晓寿司（望京soho店）
+                </p>
+                <p class="merchant-pos">
+                  望京
+                </p>
+                <p class="price"><b>￥</b>98.0</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- 使用脚步组件 -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+// 引入面包屑导航
 import DetailTopNav from '@/components/DetailTopNav'
+// 引入头部脚部组件
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 export default {
   name: 'Detail',
   components: {
+    // 注册组件
     DetailTopNav,
+    Header,
+    Footer,
   },
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+@import url('./css/Mixins.less');
+.detailContainer {
+  .content();
+  .left {
+    float: left;
+    width: 950px;
+    .filter {
+      border: 1px solid #e5e5e5;
+      background-color: #fff;
+      border-radius: 5px;
+      .condition {
+        padding: 10px 0 0 20px;
+        color: #333;
+        font-size: 14px;
+        &:last-of-type .con {
+          border-bottom: none;
+        }
+        .classification {
+          float: left;
+          width: 65px;
+        }
+        .con {
+          float: left;
+          width: 788px;
+          border-bottom: 1px solid #e5e5e5;
+          li {
+            float: left;
+            height: 28px;
+            min-width: 125px;
+            cursor: pointer;
+            a {
+              padding: 0 10px;
+            }
+          }
+          .clearfix();
+        }
+
+        .all {
+          float: left;
+          width: 53px;
+          min-width: auto;
+          text-align: center;
+          margin: 0 13px 0 9px;
+          a {
+            display: block;
+            color: #333;
+          }
+        }
+        .active {
+          background-color: #13d1be;
+          color: #fff !important;
+          border-radius: 100px;
+        }
+        a {
+          color: #666;
+        }
+        .clearfix();
+      }
+    }
+    .list {
+      border: 1px solid #e5e5e5;
+      background-color: #fff;
+      border-radius: 5px;
+      margin-top: 14px;
+    }
+  }
+  .right {
+    float: right;
+    width: 230px;
+
+    .guess-you-like {
+      border: 1px solid #e5e5e5;
+      border-radius: 5px;
+      background-color: #fff;
+      padding: 16px 20px 0;
+      h3 {
+        font-size: 16px;
+        line-height: 22px;
+        margin-bottom: 12px;
+        color: #333;
+        font-weight: 600;
+      }
+      .pic {
+        height: 106px;
+        border-radius: 5px;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 5px;
+        }
+      }
+      .merchant-name {
+        width: 100%;
+        // 禁止换行
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        font-size: 14px;
+        line-height: 20px;
+        color: #222;
+        margin: 10px 0 4px;
+      }
+      .merchant-pos {
+        font-size: 12px;
+        line-height: 17px;
+        color: #999;
+      }
+      .price {
+        font-size: 22px;
+        color: #f60;
+        line-height: 27px;
+        b {
+          font-weight: normal;
+          font-size: 14px;
+        }
+      }
+    }
+  }
+  .clearfix();
+}
+</style>
