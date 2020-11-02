760ea7645f83c8cae8fae5156c505b5b6bf348e5
liukaikai <liukaikai_web@163.com>
2020-11-02 20:10:07 +0800

    header

--- meituan/src/components/Header/index.vue
+++ meituan/src/components/Header/index.vue
@@ -4,9 +4,10 @@
 <!--    头部顶端的导航条-->
     <div class="headerBar">
       <div class="leftHeaderBar">
-        
+
       </div>
       <div class="rightHeaderBar"></div>
+
     </div>
 
   </div>
@@ -18,4 +19,28 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .header{
+      width: 100%;
+      height: 75px;
+      background: pink;
+      .headerBar{
+          width: 1190px;
+          height: 75px;
+          margin: 0 auto;
+          background: skyblue;
+          .leftHeaderBar{
+              width: 300px;
+              height: 75px;
+              float: left;
+              background: red;
+          }
+          .rightHeaderBar{
+              width: 300px;
+              height: 75px;
+              float: right;
+              background: yellow;
+          }
+      }
+  }
+</style>
