4a5c68570da15b324ec030c1b579b953e686db77
小虎 <2418293578@qq.com>
2020-11-04 21:32:59 +0800

    '第三次提交'

--- meituan/src/pages/maoyanshouye/index.vue
+++ meituan/src/pages/maoyanshouye/index.vue
@@ -2,6 +2,13 @@
   <div>
     <maoyanHeader></maoyanHeader>
     <!-- 中间部分 -->
+    <div class="banner">
+      <div class="banner-two">
+        <a href="javascript:;">
+          <img src="@/assets/xiaohuImages/banner.png" alt="">
+        </a>
+      </div>
+    </div>
     <div class="container">
       <div class="content">
         <!-- 主页面 -->
@@ -339,9 +346,132 @@
         </div>
         <!-- 侧边栏 -->
         <div class="aside">
+          <!-- 侧边栏第一个 -->
           <div class="panel">
             <div class="panel-header">
-              <div class="panel-content"></div>
+              <span class="panel-title">
+                <h2>今日票房</h2>
+              </span>
+            </div>
+            <div class="panel-content">
+              <ul class="ranking-box">
+                <li class="ranking-item" style="cursor:pointer">
+                  <div class="ranking-top-left">
+                    <img src="@/assets/xiaohuImages/xiaotu.jpg" alt="">
+                  </div>
+                  <div class="ranking-top-right">
+                    <span class="ranking-top-moive-name">金刚川</span>
+                    <span class="stonefont">267.97万</span>
+                  </div>
+                </li>
+                <li class="ranking-item2" style="cursor:pointer">
+                  <span class="normal-link">
+                    <i>2</i>
+                    <span class="zhong">数码宝贝：最后的进化</span>
+                  </span>
+                  <span class="ranking-num-info">59.95万</span>
+                </li>
+                <li class="ranking-item3" style="cursor:pointer">
+                  <span class="normal-link">
+                    <i>3</i>
+                    <span class="zhong">我和我的家乡</span>
+                  </span>
+                  <span class="ranking-num-info">57.72万</span>
+                </li>
+                <li class="ranking-item4" style="cursor:pointer">
+                  <span class="normal-link">
+                    <i>4</i>
+                    <span class="zhong">夺冠</span>
+                  </span>
+                  <span class="ranking-num-info">25.40万</span>
+                </li>
+                <li class="ranking-item5" style="cursor:pointer">
+                  <span class="normal-link">
+                    <i>5</i>
+                    <span class="zhong">一点就到家</span>
+                  </span>
+                  <span class="ranking-num-info">13.97万</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!--今日大盘  -->
+          <div class="box-total-wrapper">
+            <div class="jinri">
+              <div class="jinri2">
+                <h3>今日大盘</h3>
+              </div>
+            </div>
+            <div>
+              <p class="gengduo">
+                <span class="num">
+                  <span class="stonefont">478.7</span>
+                  <span class="num2">万</span>
+                </span>
+                <a href="javascript:;">查看更多</a>
+              </p>
+              <p class="meta-info">
+                <span class="pull-left">北京时间 08：38：05</span>
+                <span class="pull-right">猫眼专业版实时票房数据</span>
+              </p>
+            </div>
+          </div>
+          <!-- 侧边栏第二个 -->
+          <div class="cebian-second-panel">
+            <div class="panel-header">
+              <span class="panel-1">最受期待</span>
+              <span class="panel-2">查看完整榜单</span>
+            </div>
+            <div class="panel-content">
+              <ul class="ranking-wrapper">
+                <li class="ranking-item">
+                  <div class="hezi">
+                    <a href="javascript:;">
+                      <div class="ranking-top-left">
+                        <img src="@/assets/xiaohuImages/wennuan.jpg" alt="">
+                      </div>
+                      <div class="ranking-top-right">
+                        <div class="ranking-top-right-main">
+                          <span class="ranking-movie-name">温暖的抱抱</span>
+                          <p class="ranking-release-wish">上映时间：2020-12-31</p>
+                          <p class="ranking-top-wish">70991人想看</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div> 
+                </li>
+                <!-- ul 2 -->
+                <li class="second-2">
+                  <div class="min-1">
+                    <a href="javascript:;">
+                      <img src="@/assets/xiaohuImages/tangren.jpg" alt="">
+                    </a>
+                    <div class="box-1" style="cursor:pointer">
+                      <span class="dibu1">唐人街探案3</span>
+                      <span class="dibu2">2897643人想看</span>
+                    </div>
+                  </div>
+                  <div class="min-2">
+                    <a href="javascript:;">
+                      <img src="@/assets/xiaohuImages/songni.jpg" alt="">
+                    </a>
+                    <div class="box-2" style="cursor:pointer">
+                      <span class="dibu3">送你一朵小红花</span>
+                      <span class="dibu4">1647997人想看</span>
+                    </div>
+                  </div>
+                </li>
+                <!-- ul 3 -->
+                <li class="third-3">
+
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
           </div>
         </div>
@@ -361,84 +491,136 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.container{
+.container {
   width: 100%;
   height: 3300px;
   // background-color: black;
 }
-.container .content{
+.container .content {
   height: 100%;
   width: 1200px;
   // background-color: blue;
   margin: 0 auto;
   margin-top: 60px;
+  position: relative;
 }
+// banner大图
+.banner {
+  width: 1903px;
+  height: 440px;
+  margin: 0 auto;
+}
+.banner-two {
+  width: 1600px;
+  height: 440px;
+  margin: 0 auto;
+}
+.banner-two img {
+  margin-left: 90px;
+}
+
 // 中间区第一个的头部
-.content .main .movie-grid .panel .panel-header .panel-title{
+.content .main .movie-grid .panel .panel-header .panel-title {
   font-size: 26px;
   font-weight: 400;
   color: #ef4238;
 }
-.content .main .movie-grid .panel .panel-header .panel-more a{
+.content .main .movie-grid .panel .panel-header .panel-more a {
   font-size: 14px;
   color: #ef4238;
   margin-left: 500px;
 }
 // 中间区第一个的电影图片区
-.content .main .movie-grid .panel .panel-content{
+.content .main .movie-grid .panel .panel-content {
   width: 750px;
   height: 569px;
   margin-top: 23px;
 }
 
-.content .main .movie-grid .panel .panel-content .move-list dd{
+.content .main .movie-grid .panel .panel-content .move-list dd {
   display: inline-block;
   width: 160px;
   height: 220px;
   margin: 0 0 70px 22px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-title{
+.content .main .movie-grid .panel .panel-content .move-list .move-title {
   font-size: 16px;
   color: white;
   margin-top: -35px;
   margin-left: 15px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .move-detail{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .move-detail {
   width: 160px;
   height: 39px;
-  border: 1px solid gray; 
+  border: 1px solid gray;
   margin-top: 10px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .move-detail:hover{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .move-detail:hover {
   background-color: #ef4238;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .move-detail a{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .move-detail
+  a {
   display: block;
   color: #ef4238;
   text-align: center;
   line-height: 32px;
   font-size: 14px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .move-detail a:hover{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .move-detail
+  a:hover {
   color: white;
 }
 // 第二部分区域
-.content .main .movie-grid .kong{
+.content .main .movie-grid .kong {
   width: 100%;
   height: 60px;
 }
-.content .main .movie-grid .panel .panel-header .second-title{
+.content .main .movie-grid .panel .panel-header .second-title {
   font-size: 26px;
   font-weight: 400;
   color: #2d98f3;
-  
 }
-.content .main .movie-grid .panel .panel-header .second-more a{
+.content .main .movie-grid .panel .panel-header .second-more a {
   font-size: 14px;
   color: #2d98f3;
   margin-left: 500px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .second-detail{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .second-detail {
   width: 160px;
   height: 34px;
   background-color: #fbfbfb;
@@ -446,51 +628,64 @@ export default {
   border-left: 1px solid #999;
   border-right: 1px solid #999;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .second-detail span{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .second-detail
+  span {
   display: block;
   color: #ffb400;
   margin-left: 10px;
   line-height: 35px;
   font-size: 14px;
 }
-.content .main .movie-grid .panel .panel-content .move-list .move-item .presale{
+.content
+  .main
+  .movie-grid
+  .panel
+  .panel-content
+  .move-list
+  .move-item
+  .presale {
   width: 160px;
   height: 39px;
   border-left: 1px solid #999;
   border-right: 1px solid #999;
   border-bottom: 1px solid #999;
-  
 }
-.second-poster{
+.second-poster {
   margin-top: 75px;
 }
-.move-list .move-item .yugao{
+.move-list .move-item .yugao {
   font-size: 14px;
   color: #999;
   margin-left: 20px;
-
 }
-.move-list .move-item .yushou{
+.move-list .move-item .yushou {
   font-size: 14px;
   color: #999;
   margin-left: 40px;
 }
 // 第三部分
-.kong2{
+.kong2 {
   width: 100%;
   height: 200px;
 }
-.panel-header .xiao{
+.panel-header .xiao {
   margin-left: 10px;
 }
-.panel-header .xiao a{
+.panel-header .xiao a {
   font-size: 14px;
   color: #333;
 }
-.panel-header .you-more{
+.panel-header .you-more {
   margin-left: 420px;
 }
-.panel-header .you-more a{
+.panel-header .you-more a {
   color: #ef4238;
 }
 
@@ -498,7 +693,318 @@ export default {
   width: 350px;
   height: 220px;
 }
-.last-poster{
+.last-poster {
   margin-top: -40px;
+}
+// 侧边栏第一个
+.aside {
+  width: 360px;
+  height: 3300px;
+  background-color: #fff;
+  position: absolute;
+  top: 585px;
+  left: 1190px;
+}
+// 头部
+.aside .panel-header h2 {
+  font-size: 26px;
+  font-weight: 400;
+  color: #ef4238;
+}
+// 1
+.panel .ranking-item {
+  width: 360px;
+  height: 80px;
+  margin-top: 18px;
+  overflow: hidden;
+  border: 1px solid #efefef;
+  position: relative;
+}
+.panel .ranking-item:hover {
+  background-color: #efefef;
+}
+.panel .ranking-item .ranking-top-moive-name {
+  margin-top: -65px;
+  margin-left: 130px;
+  position: absolute;
+  font-size: 18px;
+  color: #333;
+}
+.stonefont {
+  position: absolute;
+  margin-left: 130px;
+  margin-top: -35px;
+  font-size: 14px;
+  color: #ef4238;
+}
+// 2
+.ranking-item2 {
+  width: 360px;
+  height: 55px;
+}
+.ranking-item2 i {
+  color: #ef4238;
+  font-size: 18px;
+  line-height: 55px;
+}
+.ranking-item2 .zhong {
+  font-size: 16px;
+  color: #333;
+  margin-left: 15px;
+}
+.ranking-item2 .ranking-num-info {
+  font-size: 14px;
+  color: #ef4238;
+  float: right;
+  line-height: 55px;
+}
+.ranking-item2:hover {
+  background-color: #efefef;
+}
+// 3
+.ranking-item3 {
+  width: 360px;
+  height: 55px;
+}
+.ranking-item3 i {
+  color: #ef4238;
+  font-size: 18px;
+  line-height: 55px;
+}
+.ranking-item3 .zhong {
+  font-size: 16px;
+  color: #333;
+  margin-left: 15px;
+}
+.ranking-item3 .ranking-num-info {
+  font-size: 14px;
+  color: #ef4238;
+  float: right;
+  line-height: 55px;
+}
+.ranking-item3:hover {
+  background-color: #efefef;
+}
+// 4
+.ranking-item4 {
+  width: 360px;
+  height: 55px;
+}
+.ranking-item4 i {
+  color: #ef4238;
+  font-size: 18px;
+  line-height: 55px;
+}
+.ranking-item4 .zhong {
+  font-size: 16px;
+  color: #333;
+  margin-left: 15px;
+}
+.ranking-item4 .ranking-num-info {
+  font-size: 14px;
+  color: #ef4238;
+  float: right;
+  line-height: 55px;
+}
+.ranking-item4:hover {
+  background-color: #efefef;
+}
+// 5
+.ranking-item5 {
+  width: 360px;
+  height: 55px;
+}
+.ranking-item5 i {
+  color: #ef4238;
+  font-size: 18px;
+  line-height: 55px;
+}
+.ranking-item5 .zhong {
+  font-size: 16px;
+  color: #333;
+  margin-left: 15px;
+}
+.ranking-item5 .ranking-num-info {
+  font-size: 14px;
+  color: #ef4238;
+  float: right;
+  line-height: 55px;
+}
+.ranking-item5:hover {
+  background-color: #efefef;
+}
+// 今日大盘
+.box-total-wrapper {
+  width: 360px;
+  height: 105px;
+  margin-top: 30px;
+  border: 1px solid #efefef;
+}
+.box-total-wrapper .jinri {
+  width: 43px;
+  height: 105px;
+  background-color: #ef4238;
+}
+.box-total-wrapper .jinri2 {
+  width: 23px;
+  height: 105px;
+  background-color: #ef4238;
+}
+.box-total-wrapper h3 {
+  color: #fff;
+  font-size: 17px;
+  margin-left: 12px;
+  padding-top: 7px;
+}
+.box-total-wrapper .stonefont {
+  margin-left: 50px;
+  font-size: 30px;
+  font-weight: 700;
+  margin-top: -80px;
+}
+.box-total-wrapper .num {
+  position: relative;
+}
+.box-total-wrapper .num2 {
+  margin-left: 140px;
+  position: absolute;
+  margin-top: -67px;
+  color: #ef4238;
+}
+.box-total-wrapper .gengduo {
+  position: relative;
+}
+.box-total-wrapper a {
+  position: absolute;
+  margin-top: -67px;
+  margin-left: 260px;
+  color: #ef4238;
+  font-size: 14px;
+}
+.box-total-wrapper .meta-info {
+  position: relative;
+}
+.meta-info .pull-left {
+  position: absolute;
+  color: #999;
+  font-size: 14px;
+  margin-left: 50px;
+  margin-top: -37px;
+}
+.meta-info .pull-right {
+  position: absolute;
+  color: #999;
+  font-size: 14px;
+  margin-left: 195px;
+  margin-top: -37px;
+}
+// 侧边栏第二个
+.cebian-second-panel {
+  width: 360px;
+  height: 855px;
+  margin-top: 85px;
+}
+.cebian-second-panel .panel-header {
+  width: 360px;
+  height: 26px;
+}
+.cebian-second-panel .panel-1 {
+  font-size: 26px;
+  font-weight: 400;
+  color: #ffb400;
+}
+.cebian-second-panel .panel-2 {
+  font-size: 14px;
+  color: #ffb400;
+  float: right;
+  line-height: 40px;
+}
+// ul 1
+.cebian-second-panel .ranking-item {
+  width: 360px;
+  height: 194px;
+  margin-top: 31px;
+  position: relative;
+}
+.cebian-second-panel .ranking-item:hover {
+  background-color: #efefef;
+}
+.cebian-second-panel .ranking-item .ranking-movie-name{
+  position: absolute;
+  font-size: 18px;
+  color: #333;
+  margin-left: 150px;
+  margin-top: -140px;
+}
+.cebian-second-panel .ranking-item .ranking-release-wish{
+  position: absolute;
+  color: #999;
+  margin-left: 150px;
+  margin-top: -100px;
+}
+.cebian-second-panel .ranking-item .ranking-top-wish{
+  position: absolute;
+  color: #ffb400;
+  font-size: 14px;
+  margin-left: 150px;
+  margin-top: -60px;
+}
+// ul 2
+.cebian-second-panel .second-2{
+  width: 360px;
+  height: 180px;
+  margin-top: 20px;
+}
+.cebian-second-panel .min-1{
+  float: left;
+  width: 170px;
+  height: 180px;
+}
+.cebian-second-panel .min-1:hover{
+  background-color: #efefef;
+}
+.cebian-second-panel .min-2{
+  float: right;
+  width: 170px;
+  height: 180px;
+}
+.cebian-second-panel .min-2:hover{
+  background-color: #efefef;
+}
+.cebian-second-panel .box-1{
+  width: 170px;
+  height: 60px;
+  margin-top: -4px;
+  border: 1px solid #efefef;
+ 
+}
+.cebian-second-panel .dibu1{
+  font-size: 18px;
+  margin-left: 10px;
+   display: block;
+   margin-top: 10px;
+}
+.cebian-second-panel .dibu2{
+  font-size: 14px;
+  color: #fdb863;
+  margin-left: 10px;
+}
+.cebian-second-panel .box-2{
+  width: 170px;
+  height: 60px;
+  margin-top: -4px;
+  border: 1px solid #efefef;
+ 
+}
+.cebian-second-panel .dibu3{
+  font-size: 18px;
+  margin-left: 10px;
+   display: block;
+   margin-top: 10px;
+}
+.cebian-second-panel .dibu4{
+  font-size: 14px;
+  color: #fdb863;
+  margin-left: 10px;
 }
 </style>
