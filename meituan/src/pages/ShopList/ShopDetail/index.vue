4ac3570539b2c305ebe961170a391499b06033b3
shuo <1115464444@qq.com>
2020-11-06 19:14:08 +0800

    店铺详情页动态交互80%

--- meituan/src/pages/ShopList/ShopDetail/index.vue
+++ meituan/src/pages/ShopList/ShopDetail/index.vue
@@ -61,7 +61,15 @@
           </div>
           <!-- 地址 -->
           <div class="address">
-            <p>{{ baseInfo.address }} <i class="iconfont iconweizhi"></i></p>
+            <p>
+              {{ baseInfo.address }}
+              <i class="iconfont iconweizhi" @click="isShowMap">
+                <div class="mapContainer" v-if="isMap">
+                  <i class="iconfont iconshanchu" @click.stop="isMap = false"></i>
+                  <Map :position="position" :label="label"></Map>
+                </div>
+              </i>
+            </p>
             <p>电话：{{ baseInfo.phone }}</p>
             <p>营业时间：{{ baseInfo.openTime }}</p>
           </div>
@@ -85,11 +93,20 @@
           <div class="big-img">
             <img :src="photos.frontImgUrl" alt="" />
           </div>
-          <ul class="img-list">
+
+          <!-- 轮播图组件 -->
+          <div v-swiper:mySwiper="swiperOption" @click="isMask = true">
+            <ul class="swiper-wrapper img-list">
+              <li class="swiper-slide" v-for="(item, index) in photos.albumImgUrls" :key="index">
+                <img :src="item" />
+              </li>
+            </ul>
+          </div>
+          <!-- <ul class="img-list">
             <li v-for="(item, index) in photos.albumImgUrls" :key="index">
               <img :src="item" alt="" />
             </li>
-          </ul>
+          </ul> -->
         </div>
       </div>
       <!-- 详情底部 -->
@@ -139,15 +156,15 @@
             <!-- 网友点评内容区 -->
             <div class="comment-con">
               <!-- 网友点评标签区 -->
-              <ul class="tags">
-                <li v-for="(item, index) in tags" :key="item.tag">
-                  <span>{{ item.tag }}</span>
-                  <i>({{ item.count }})</i>
+              <ul class="tags" @click="cutTags">
+                <li v-for="(item, index) in tags" :key="item.tag" :class="{ active: index === tagsIndex }">
+                  <span :data-tagsIndex="index">{{ item.tag }}</span>
+                  <i :data-tagsIndex="index">({{ item.count }})</i>
                 </li>
               </ul>
               <!-- 网友点评查看图片评论 -->
               <div class="sea" @click="seaImgComment">
-                <!--  -->
+                <!-- 选中未选中 -->
                 <i class="iconfont" :class="{ iconweixuanzhong: !isSea, iconyixuanzhong: isSea }"></i>
                 <span>只看有图片的评论</span>
               </div>
@@ -244,11 +261,11 @@
         <div class="btm-right">
           <div class="guess-you-like">
             <h3>猜你喜欢</h3>
-            <ul>
+            <ul @click="changeGuessInfo">
               <li v-for="(item, index) in guessInfo" :key="item.itemId">
                 <a href="javascript:;">
                   <div class="pic">
-                    <img :src="item.imgUrl" alt="" />
+                    <img :src="item.imgUrl" alt="" :data-shopId="item.itemId" />
                   </div>
                   <p class="merchant-name">
                     {{ item.title }}
@@ -268,148 +285,43 @@
         <h4>附近商家</h4>
         <div class="nearby">
           <ul>
-            <li>
+            <li v-for="item in shopDetailData.nearPoiList" :key="item.sold">
               <a href="javascript:;">
                 <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
+                  <img :src="item.headIcon" alt="" />
                 </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
+                <p class="title">
+                  {{ item.name }}
+                </p>
+                <p class="source">
+                  {{ item.score }}分 <span>{{ item.id }}人消费</span>
+                </p>
                 <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <div class="pic">
-                  <img
-                    src="https://p0.meituan.net//bbia/3e36af692ce40ea56575b7c19dc3581c464792.png@214w_120h_1e_1c"
-                    alt=""
-                  />
-                </div>
-                <p class="title">P.STYLE 派斯造型（五道口店）</p>
-                <p class="source">5分 <span>188792人消费</span></p>
-                <p class="desc source">免费停车</p>
-                <p class="price"><b>￥</b> 766 <b>起</b></p>
+                <p class="price"><b>￥</b> {{ item.avgPrice }} <b>起</b></p>
               </a>
             </li>
           </ul>
         </div>
+      </div>
+    </div>
+    <!-- 蒙版轮播 -->
+    <div class="mask" :style="{ display: isMask ? 'block' : 'none' }">
+      <div class="thumb-example">
+        <div class="closeMask" @click="isMask = false">X</div>
+        <!-- swiper1 -->
+        <swiper class="swiper gallery-top" :options="swiperOptionTop" ref="swiperTop">
+          <swiper-slide class="slide-1" v-for="(item, index) in photos.albumImgUrls" :key="index">
+            <img :src="item" />
+          </swiper-slide>
+          <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+          <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+        </swiper>
+        <!-- swiper2 Thumbs -->
+        <swiper class="swiper gallery-thumbs" :options="swiperOptionThumbs" ref="swiperThumbs">
+          <swiper-slide class="slide-1" v-for="(item, index) in photos.albumImgUrls" :key="index">
+            <img :src="item" />
+          </swiper-slide>
+        </swiper>
       </div>
     </div>
   </div>
@@ -419,12 +331,24 @@
 // 引入时间初始化工具
 import moment from 'moment'
 import 'moment/locale/zh-cn'
+// 引入轮播图组件
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+// 引入地图组件
+import Map from '@/pages/ShopList/Map'
+// 引入面包屑组件
 import Breadcrumbs from '@/components/Breadcrumbs'
 import { mapState } from 'vuex'
 export default {
   name: 'ShopDetail',
   components: {
     Breadcrumbs,
+    Swiper,
+    SwiperSlide,
+    Map,
+  },
+  directives: {
+    swiper: directive,
   },
   data() {
     return {
@@ -435,6 +359,37 @@ export default {
       sortCurrentClass: 0,
       commentsList: [],
       isSea: false,
+      tagsIndex: -1,
+      // 轮播图配置
+      swiperOption: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        loopFillGroupWithBlank: true,
+      },
+      // 点击轮播图进入的蒙版轮播图配置
+      swiperOptionTop: {
+        // loop: true,
+        loopedSlides: 1, // looped slides should be the same
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+      swiperOptionThumbs: {
+        // loop: true,
+        loopedSlides: 1, // looped slides should be the same
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true,
+      },
+      // 蒙版轮播图开关
+      isMask: false,
+      isMap: false,
+      position: [],
+      label: {},
     }
   },
   // 管道函数 格式化时间
@@ -447,6 +402,11 @@ export default {
     },
   },
   methods: {
+    isShowMap() {
+      this.isMap = true
+      // 初始化地图数据
+      this.initMapData()
+    },
     // 每页 ${val} 条
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`)
@@ -461,13 +421,18 @@ export default {
     },
     // 发送请求店铺获取数据
     getShopDetailData() {
+      const shopId = this.shopId
+      this.$store.dispatch('getShopDetailData', { shopId })
+    },
+    // 初始化 shopId
+    initShopId() {
       const { shopId } = this.$route.params
       this.shopId = shopId
-      this.$store.dispatch('getShopDetailData', { shopId })
     },
     // 获取店铺评论信息数据
     getMerchantComment() {
       let { shopId, isSea } = this
+
       isSea = isSea ? '1' : '0'
       this.$store.dispatch('getMerchantComment', { shopId, isSea })
     },
@@ -489,14 +454,59 @@ export default {
       // 调用获取参数评论信息函数
       this.getMerchantComment()
     },
+    // 切换用户评论标签
+    cutTags(e) {
+      const target = e.target
+      const { tagsindex: tagsIndex } = target.dataset
+      this.tagsIndex = tagsIndex * 1
+    },
+    // 猜你喜欢路由跳转
+    changeGuessInfo(e) {
+      const target = e.target
+      const { shopid: shopId } = target.dataset
+      this.shopId = shopId
+      const location = {
+        name: 'shopDetail',
+        params: {
+          shopId,
+        },
+      }
+      // 跳转路由
+      this.$router.push(location)
+      // 初始化 shopId
+      this.initShopId()
+      // 获取店铺详细信息
+      this.getShopDetailData()
+    },
+    // 初始化地图数据
+    initMapData() {
+      this.position.push(this.baseInfo.longitude, this.baseInfo.latitude)
+      const label = {
+        direction: 'top',
+      }
+      label.content = this.baseInfo.name
+      this.$set(this.$data, 'label', label)
+    },
   },
   mounted() {
+    // 初始化shopId
+    this.initShopId()
     // 调用 获取猜你喜欢数据函数
     this.getGuessInfo()
     // 调用 获取列表信息函数
     this.getShopDetailData()
     // 调用获取参数评论信息函数
     this.getMerchantComment()
+
+    // 轮播图
+    this.mySwiper.slideTo(3, 1000, false)
+    // 轮播图蒙版
+    this.$nextTick(() => {
+      const swiperTop = this.$refs.swiperTop.$swiper
+      const swiperThumbs = this.$refs.swiperThumbs.$swiper
+      swiperTop.controller.control = swiperThumbs
+      swiperThumbs.controller.control = swiperTop
+    })
   },
   computed: {
     // 从vuex 中把商品筛选数据拿出来
@@ -624,6 +634,24 @@ export default {
             margin-bottom: 10px;
             .iconweizhi {
               color: #00c9b3;
+              position: relative;
+              cursor: pointer;
+              .mapContainer {
+                position: absolute;
+                right: -315px;
+                top: -5px;
+                border: 5px solid #ccc;
+                .iconfont {
+                  font-size: 24px;
+                  position: absolute;
+                  right: -40px;
+                  width: 35px;
+                  height: 35px;
+                  text-align: center;
+                  top: -5px;
+                  line-height: 35px;
+                }
+              }
             }
           }
         }
@@ -669,7 +697,7 @@ export default {
         // 小图列表区域
         .img-list {
           width: 100%;
-          overflow: hidden;
+          // overflow: hidden;
           padding-top: 10px;
           display: flex;
 
@@ -844,6 +872,11 @@ export default {
                 border: 1px solid #e5e5e5;
                 line-height: 34px;
                 margin: 0 5px 10px;
+                &.active {
+                  background-color: #00c9b3;
+                  border: 1px solid #00c9b3;
+                  color: #fff;
+                }
                 i {
                   font-style: normal;
                 }
@@ -1125,6 +1158,57 @@ export default {
             }
           }
         }
+      }
+    }
+  }
+  // 蒙版轮播图
+  .mask {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    display: none;
+    // 缩略图
+    .thumb-example {
+      position: relative;
+      width: 60%;
+      text-align: center;
+      max-height: 637px;
+      margin: 10% auto 0;
+      .closeMask {
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.1);
+        border-radius: 50%;
+        position: absolute;
+        right: 30px;
+        color: #fff;
+        z-index: 99;
+      }
+    }
+    // 缩略图
+    .swiper {
+      &.gallery-top {
+        height: 80%;
+        width: 100%;
+      }
+      &.gallery-thumbs {
+        height: 20%;
+        box-sizing: border-box;
+        padding: 10px 0;
+      }
+      &.gallery-thumbs .swiper-slide {
+        width: 25%;
+        height: 100%;
+        opacity: 0.4;
+      }
+      &.gallery-thumbs .swiper-slide-active {
+        opacity: 1;
       }
     }
   }
