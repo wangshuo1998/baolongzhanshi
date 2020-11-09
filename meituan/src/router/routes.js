43f251846ae13299ef538a6ef55f6cadd3282404
shuo <1115464444@qq.com>
2020-11-09 13:32:42 +0800

    民宿路由添加

--- meituan/src/router/routes.js
+++ meituan/src/router/routes.js
@@ -1,50 +1,50 @@
 /*
-* import Register from "@/pages/Register"; 为正常加载
-* const Home = ()=>import('@/pages/Home') 懒加载路由 提高效率
-*
-* */
+ * import Register from "@/pages/Register"; 为正常加载
+ * const Home = ()=>import('@/pages/Home') 懒加载路由 提高效率
+ *
+ * */
 // import Register from "@/pages/Register";
-const Register = ()=>import('@/pages/Register')
+const Register = () => import('@/pages/Register')
 // import Home from '@/pages/Home'
-const Home = ()=>import('@/pages/Home')
+const Home = () => import('@/pages/Home')
 
 // import HomeIndex from '@/pages/HomeIndex'
-const HomeIndex = ()=>import('@/pages/HomeIndex')
+const HomeIndex = () => import('@/pages/HomeIndex')
 
 // import ShopList from '@/pages/ShopList'
-const ShopList = ()=>import('@/pages/ShopList')
+const ShopList = () => import('@/pages/ShopList')
 
 // import ShopDetail from '@/pages/ShopList/ShopDetail'
-const ShopDetail = ()=>import('@/pages/ShopList/ShopDetail')
+const ShopDetail = () => import('@/pages/ShopList/ShopDetail')
 
 // import ShopListIndex from '@/pages/ShopList/ShopListIndex'
-const ShopListIndex = ()=>import('@/pages/ShopList/ShopListIndex')
+const ShopListIndex = () => import('@/pages/ShopList/ShopListIndex')
 
 
 // import ChangeCity from "@/pages/ChangeCity"
-const ChangeCity = ()=>import('@/pages/ChangeCity')
+const ChangeCity = () => import('@/pages/ChangeCity')
 
 
 // import Login from "@/pages/Login"
-const Login = ()=>import('@/pages/Login')
+const Login = () => import('@/pages/Login')
 
 // import Rules from '@/pages/Rules'
-const Rules = ()=>import('@/pages/Rules')
+const Rules = () => import('@/pages/Rules')
 
 // import FastFood from '@/pages/FastFood'
-const FastFood = ()=>import('@/pages/FastFood')
+const FastFood = () => import('@/pages/FastFood')
 
 // import HomestayInfo from '@/pages/HomestayInfo'
-const HomestayInfo = ()=>import('@/pages/HomestayInfo')
+const HomestayInfo = () => import('@/pages/HomestayInfo')
 
 // import Catalogue from '@/pages/Catalogue'
-const Catalogue = ()=>import('@/pages/Catalogue')
+const Catalogue = () => import('@/pages/Catalogue')
 
 // import Homestay from '@/pages/Homestay'
-const Homestay = ()=>import('@/pages/Homestay')
+const Homestay = () => import('@/pages/Homestay')
 
 // import maoyanshouye from '@/pages/maoyanshouye'
-const maoyanshouye = ()=>import('@/pages/maoyanshouye')
+const maoyanshouye = () => import('@/pages/maoyanshouye')
 
 export default [{
     path: '/',
@@ -81,7 +81,7 @@ export default [{
   },
   {
     path: "/login",
-    name:'login',
+    name: 'login',
     component: Login
   },
   {
